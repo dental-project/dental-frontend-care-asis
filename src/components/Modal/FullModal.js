b3d636590b2dc49f9c5a48ee6716bd7422dd0170
HyukHyunCho <gurgus0189@gmail.com>
2022-01-12 18:00:45 +0900

    Full modal update

--- src/components/Modal/FullModal.js
+++ src/components/Modal/FullModal.js
@@ -14,6 +14,9 @@ import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
 
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
 import Grid from '@material-ui/core/Grid';
 
 import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
@@ -22,6 +25,9 @@ import TextField from "@material-ui/core/TextField";
 import { useForm, Controller } from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   appBar: {
     position: 'relative',
   },
@@ -30,9 +36,7 @@ const useStyles = makeStyles((theme) => ({
     flex: 1,
   },
   textField: {
-    float: "left",
-    width: "23%",
-    marginTop: "20px",
+    width: "100%",
     margin: theme.spacing(1),
     '& label.Mui-focused': {
         color: '#00acc1',
@@ -45,7 +49,7 @@ const useStyles = makeStyles((theme) => ({
   },
   textField2: {
     float: "left",
-    width: "47%",
+    //width: "47%",
     marginTop: "20px",
     margin: theme.spacing(1),
     '& label.Mui-focused': {
@@ -92,6 +96,9 @@ export default function FullScreenDialog(props) {
     stringify: (option) => option.title,
   });
 
+
+
+
   const dash1 = [ { title: "마포한그루" }, { title: "연세유라인" }, { title: "미소유" }, { title: "연세미엘치과" }, { title: "연세키즈사랑" }, { title: "연세윤치과" }, 
                   { title: "군포 에미담치과" }, { title: "연세키즈투틴치과" }, { title: "중구강약안면외과" }, { title: "라임프로" }, { title: "연세후" }, 
                   { title: "스노우화이트" }, { title: "과천 연세스위트" }, { title: "연세두리치과" }, { title: "서울늘편한" }, { title: "이바른치과" }, { title: "약수 연세치과" }, ];
@@ -123,59 +130,270 @@ export default function FullScreenDialog(props) {
 
 
 
-            <Grid container justifyContent="center" spacing={spacing}>
-                <TextField
+           
+              <Grid container spacing={1} >
+                <Grid item xs={4}>
+                  <TextField
                     id="date"
                     label="접수일자"
                     type="date"
                     defaultValue="2022-01-11"
                     className={classes.textField}
                     InputLabelProps={{
-                    shrink: true,
+                      shrink: true,
                     }}
-                />
-                <TextField
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <TextField
                     id="date"
                     label="완성일자"
                     type="date"
                     defaultValue="2022-01-11"
                     className={classes.textField}
                     InputLabelProps={{
-                    shrink: true,
+                      shrink: true,
                     }}
-                />
-                
-                <Autocomplete
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <TextField
+                    id="date"
+                    label="배달일자"
+                    type="date"
+                    defaultValue="2022-01-11"
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <Autocomplete
                     className={classes.textField}
                     id="filter-demo"
                     options={dash1}
                     getOptionLabel={(option) => option.title}
                     filterOptions={filterOptions}
-                    renderInput={(params) => <TextField {...params} label="치과" variant="outlined" />}
-                />  
-
-                <Controller
+                    renderInput={(params) => <TextField {...params} label="거래처명" variant="outlined" />}
+                  />  
+                </Grid>
+                <Grid item xs={4}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value } }) => (
+                      <TextField
+                        className={classes.textField} 
+                        label="차트번호"
+                        variant="outlined"
+                        onChange={onChange}
+                      />
+                    )}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <Controller
                     name="partName"
                     control={control}
                     defaultValue=""
                     render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
+                      <TextField
                         className={classes.textField} 
-                        label="이름"
+                        label="환자명"
                         variant="outlined"
                         onChange={onChange}
                         error={!!error}
                         helperText={error ? error.message : null}
-                    />
+                      />
                     )}
                     rules={{ 
-                    required: "이름을 입력하세요."
+                      required: "환자명을 입력하세요."
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={2}></Grid>
+
+                <Grid item xs={2}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value } }) => (
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            className={classes.textField} 
+                            checked={true}
+                            onChange={onChange}
+                            name="upper"
+                            color="primary"
+                          />
+                        }
+                        label="Upper"
+                      />
+                    )}
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value } }) => (
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            className={classes.textField} 
+                            checked={true}
+                            onChange={onChange}
+                            name="lower"
+                            color="primary"
+                          />
+                        }
+                        label="Lower"
+                      />
+                    )}
+                  />bu
+                 </Grid>
+                <Grid item xs={2}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value } }) => (
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            className={classes.textField} 
+                            checked={true}
+                            onChange={onChange}
+                            name="bite"
+                            color="primary"
+                          />
+                        }
+                        label="Bite"
+                      />
+                    )}
+                  />
+                 </Grid>
+                <Grid item xs={2}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value } }) => (
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            className={classes.textField}
+                            checked={true}
+                            onChange={onChange}
+                            name="appliance"
+                            color="primary"
+                          />
+                        }
+                        label="장치"
+                      />
+                    )}
+                  />
+                </Grid>
+
+                <Grid item xs={2}></Grid>          
+
+
+
+
+
+
+
+                <Grid item xs={2}>
+                  <Autocomplete
+                      className={classes.textField}
+                      id="filter-demo"
+                      options={dash2}
+                      getOptionLabel={(option) => option.title}
+                      filterOptions={filterOptions}
+                      renderInput={(params) => <TextField {...params} label="파트명" variant="outlined" />}
+                  /> 
+                  
+                </Grid>    
+                <Grid item xs={2}>
+                  <Autocomplete
+                      className={classes.textField}
+                      id="filter-demo"
+                      options={dash3}
+                      getOptionLabel={(option) => option.title}
+                      filterOptions={filterOptions}
+                      renderInput={(params) => <TextField {...params} label="장치명" variant="outlined" />}
+                  />                  
+                </Grid>  
+
+                <Grid item xs={3}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                      <TextField
+                        className={classes.textField} 
+                        label="단가"
+                        variant="outlined"
+                        onChange={onChange}
+                        error={!!error}
+                        helperText={error ? error.message : null}
+                      />
+                    )}
+                    rules={{ 
+                      required: "단가를 입력하세요."
                     }}
                 />
-            </Grid>
+                </Grid> 
 
-            <Grid container justifyContent="center" spacing={spacing}>
-                <Autocomplete
+                <Grid item xs={3}>
+                  <Controller
+                    name="partName"
+                    control={control}
+                    defaultValue=""
+                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                      <TextField
+                        className={classes.textField} 
+                        label="수량"
+                        variant="outlined"
+                        onChange={onChange}
+                        error={!!error}
+                        helperText={error ? error.message : null}
+                      />
+                    )}
+                    rules={{ 
+                      required: "수량을 입력하세요."
+                  }}
+                />
+                  
+                </Grid>    
+
+                <Grid item xs={1}>
+                  <Button variant="outlined" color="secondary" style={{width: "100%",height: "74%", marginTop: "8px"}}>
+                    삭제
+                  </Button>
+                </Grid> 
+
+                <Grid item xs={1}>
+                  <Button variant="outlined" color="primary" style={{width: "100%",height: "74%", marginTop: "8px"}}>
+                    +
+                  </Button>
+                </Grid>
+
+              </Grid>
+
+
+
+
+
+              {/* <Grid item xs={4}>
+              <Autocomplete
                     className={classes.textField}
                     id="filter-demo"
                     options={dash2}
@@ -227,117 +445,12 @@ export default function FullScreenDialog(props) {
                     required: "수량을 입력하세요."
                     }}
                 />
-            </Grid>
+              </Grid> */}
 
-            <Grid container justifyContent="center" spacing={spacing}>
-                <Autocomplete
-                    className={classes.textField}
-                    id="filter-demo"
-                    options={dash2}
-                    getOptionLabel={(option) => option.title}
-                    filterOptions={filterOptions}
-                    renderInput={(params) => <TextField {...params} label="파트명" variant="outlined" />}
-                /> 
-                <Autocomplete
-                    className={classes.textField}
-                    id="filter-demo"
-                    options={dash3}
-                    getOptionLabel={(option) => option.title}
-                    filterOptions={filterOptions}
-                    renderInput={(params) => <TextField {...params} label="장치명" variant="outlined" />}
-                />
-                <Controller
-                    name="partName"
-                    control={control}
-                    defaultValue=""
-                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
-                        className={classes.textField} 
-                        label="단가"
-                        variant="outlined"
-                        onChange={onChange}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                    />
-                    )}
-                    rules={{ 
-                    required: "단가를 입력하세요."
-                    }}
-                />
-                <Controller
-                    name="partName"
-                    control={control}
-                    defaultValue=""
-                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
-                        className={classes.textField} 
-                        label="수량"
-                        variant="outlined"
-                        onChange={onChange}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                    />
-                    )}
-                    rules={{ 
-                    required: "수량을 입력하세요."
-                    }}
-                />
-            </Grid>
+            
 
-            <Grid container justifyContent="center" spacing={spacing}>
-                <Autocomplete
-                    className={classes.textField}
-                    id="filter-demo"
-                    options={dash2}
-                    getOptionLabel={(option) => option.title}
-                    filterOptions={filterOptions}
-                    renderInput={(params) => <TextField {...params} label="파트명" variant="outlined" />}
-                /> 
-                <Autocomplete
-                    className={classes.textField}
-                    id="filter-demo"
-                    options={dash3}
-                    getOptionLabel={(option) => option.title}
-                    filterOptions={filterOptions}
-                    renderInput={(params) => <TextField {...params} label="장치명" variant="outlined" />}
-                />
-                <Controller
-                    name="partName"
-                    control={control}
-                    defaultValue=""
-                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
-                        className={classes.textField} 
-                        label="단가"
-                        variant="outlined"
-                        onChange={onChange}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                    />
-                    )}
-                    rules={{ 
-                    required: "단가를 입력하세요."
-                    }}
-                />
-                <Controller
-                    name="partName"
-                    control={control}
-                    defaultValue=""
-                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
-                        className={classes.textField} 
-                        label="수량"
-                        variant="outlined"
-                        onChange={onChange}
-                        error={!!error}
-                        helperText={error ? error.message : null}
-                    />
-                    )}
-                    rules={{ 
-                    required: "수량을 입력하세요."
-                    }}
-                />
-            </Grid>
+            
+
                 {/* <div style={{align: "center"}}>test.png</div> */}
 
 
